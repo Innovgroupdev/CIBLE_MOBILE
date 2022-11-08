f4593580ecd31526fbb0e7776a497efbca39b760
shakiroye <oyeossiadewoye@gmail.com>
2022-11-08 00:43:41 +0000

    favoris provider

--- lib/providers/favorisProvider.dart
+++ lib/providers/favorisProvider.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/widgets.dart';
 
 class FavorisProvider with ChangeNotifier {
+  final String _titre = '';
+  final String _image = '';
+  final String _auteur = '';
+  final String _like = '';
+
+  String get titre => _titre;
+  String get image => _image;
+  String get auteur => _auteur;
+  String get like => _like;
+
   
 }
