e9b28e4e6fc1a641746a62eed61845aabd7dddb4
shakiroye <oyeossiadewoye@gmail.com>
2023-08-31 17:58:38 +0000

    chore: refactor

--- lib/database/notificationDBcontroller.dart
+++ lib/database/notificationDBcontroller.dart
@@ -6,7 +6,7 @@ import '../models/notification.dart';
 class NotificationDBcontroller {
   Future<void> insert(NotificationModel notification1) async {
     final Database db = await CibleDataBase().database;
-    // final notification1 = 
+    // final notification1 =
     // NotificationModel(
     //     4,
     //     'https://soutenir.gnadoe.com/wp-content/uploads/2022/06/WhatsApp-Image-2022-06-24-at-20.07.56.jpeg',
@@ -14,16 +14,16 @@ class NotificationDBcontroller {
     //     'description4',
     //     'type4',
     //     false);
-        
+
     int id = await db.insert('notification', notification1.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace);
     print('le id que je cherche ' + notification1.etat.toString());
   }
 
-    Future<void> update(NotificationModel notification) async {
+  Future<void> update(NotificationModel notification) async {
     final Database db = await CibleDataBase().database;
     await db.update('notification', notification.toMap(),
-         where: "id = ?", whereArgs: [notification.id]);
+        where: "id = ?", whereArgs: [notification.id]);
   }
 
   Future<void> delete(NotificationModel notification) async {
@@ -36,8 +36,7 @@ class NotificationDBcontroller {
     final Database db = await CibleDataBase().database;
     final List<Map<String, dynamic>> maps = await db.query('notification');
     //print('le map que je cherche ' + maps.toString());
-    
-    print('le id que je cherche1 ' + maps[0]["etat"].toString());
+
     List<NotificationModel> notification = List.generate(maps.length, (i) {
       return NotificationModel.fromMap(maps[i]);
     });
